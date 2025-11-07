b90cebfb3ba8dd1acd745f89ad469f4cf1c99d56
amprabesh1 <amprabesh@gmail.com>
2025-11-07 12:06:55 -0600

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+
+    base: mode === "production" ? "./" : "/",
+
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
